e22390e6ee41e922de0cefdf4ea7674c24f22be5
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-07-24 21:46:55 -0300

    wip creating websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { io } from 'socket.io';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    public ngOnInit(): void {
+        const socket: WebSocket = io(environment.apiUrl);
+    }
     public title: string = 'Zoppy';
 }
