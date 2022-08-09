939e52316afada6ea1f3007ba8101f462ad883ee
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-08-09 00:00:11 -0300

    Fixing tests

--- src/core/pages/register/register.component.spec.ts
+++ src/core/pages/register/register.component.spec.ts
@@ -4,6 +4,14 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { RegisterComponent } from './register.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ButtonModule } from '@lucarrloliveira/button';
+import { IconModule } from '@lucarrloliveira/icon';
+import { InputModule } from '@lucarrloliveira/input';
+import { ToastService } from '@lucarrloliveira/toast';
+import { CarrosselModule } from 'src/shared/components/carrossel/carrossel.module';
+import { CheckboxModule } from '@lucarrloliveira/checkbox';
 
 describe('RegisterComponent', () => {
     let component: RegisterComponent;
@@ -11,7 +19,9 @@ describe('RegisterComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [RegisterComponent]
+            declarations: [RegisterComponent],
+            imports: [RouterTestingModule, HttpClientTestingModule, IconModule, ButtonModule, InputModule, CarrosselModule, CheckboxModule],
+            providers: [ToastService]
         }).compileComponents();
     }));
 
