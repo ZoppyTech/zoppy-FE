0fe161ad7054c88147c82059fa055f0bd43fe95c
Lucas Roscoe <30293662+lucasroscoeoliveira@users.noreply.github.com>
2022-08-30 13:53:47 -0300

    Finish UI access tokens (#7)

---
+++ src/shared/models/requests/wc-key/wc-key.request.ts
@@ -0,0 +1,6 @@
+export class wcKeyRequest {
+    public declare key: string | number;
+    public declare secret: string | number;
+    public declare url: string | number;
+    public declare visible: boolean;
+}
