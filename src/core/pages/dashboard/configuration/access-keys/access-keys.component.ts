3e0ae427cce8177b44d17a5cc995a157d4075d99
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-08-30 11:48:12 -0300

    Finish UI access tokens

--- src/core/pages/dashboard/configuration/access-keys/access-keys.component.ts
+++ src/core/pages/dashboard/configuration/access-keys/access-keys.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { wcKeyRequest } from 'src/shared/models/requests/wc-key/wc-key.request';
 import { BreadcrumbService } from 'src/shared/services/breadcrumb/breadcrumb.service';
 import { SideMenuService } from 'src/shared/services/side-menu/side-menu.service';
 
@@ -8,6 +9,8 @@ import { SideMenuService } from 'src/shared/services/side-menu/side-menu.service
     styleUrls: ['./access-keys.component.scss']
 })
 export class AccessKeysComponent implements OnInit {
+    public key: wcKeyRequest = new wcKeyRequest();
+
     public constructor(public sideMenuService: SideMenuService, public breadcrumb: BreadcrumbService) {}
 
     public ngOnInit() {
@@ -26,5 +29,13 @@ export class AccessKeysComponent implements OnInit {
             }
         ];
         this.sideMenuService.changeSub(`access-keys`);
+    }
+
+    public async save(): Promise<void> {
+        console.log(`do nothing`);
+    }
+
+    public getSaveDisabled(): boolean {
+        return !this.key.key || !this.key.secret || !this.key.url;
     }
 }
