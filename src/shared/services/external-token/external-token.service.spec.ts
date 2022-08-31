de7fabd2e58df1d7f09fcfffe3c3b300bbee22c2
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-08-30 22:19:04 -0300

    fixing tests

--- src/shared/services/external-token/external-token.service.spec.ts
+++ src/shared/services/external-token/external-token.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ExternalTokenService } from './external-token.service';
 
 describe('Service: ExternalToken', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
             providers: [ExternalTokenService]
         });
     });
--- src/shared/services/wc-sync/wc-sync.service.spec.ts
+++ src/shared/services/wc-sync/wc-sync.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { WcSyncService } from './wc-sync.service';
 
 describe('Service: WcSync', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
             providers: [WcSyncService]
         });
     });
