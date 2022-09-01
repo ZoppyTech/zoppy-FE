1f55aa7acac9ee4b9d0d868cb8d530a07f9824c7
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-09-01 19:34:16 -0300

    My company integration completed

--- src/core/pages/dashboard/my-company/my-company.component.ts
+++ src/core/pages/dashboard/my-company/my-company.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from '@lucarrloliveira/toast';
+import { CompanyEntity } from 'src/shared/models/entities/company.entity';
+import { CompanyRequest } from 'src/shared/models/requests/company/company.request';
+import { ZoppyException } from 'src/shared/services/api.service';
 import { BreadcrumbService } from 'src/shared/services/breadcrumb/breadcrumb.service';
+import { CompanyService } from 'src/shared/services/company/company.service';
 import { SideMenuService } from 'src/shared/services/side-menu/side-menu.service';
+import { Storage } from 'src/shared/utils/storage';
 
 @Component({
     selector: 'app-my-company',
@@ -8,19 +14,49 @@ import { SideMenuService } from 'src/shared/services/side-menu/side-menu.service
     styleUrls: ['./my-company.component.scss']
 })
 export class MyCompanyComponent implements OnInit {
-    public constructor(public sideMenuService: SideMenuService, public breadcrumb: BreadcrumbService) {}
+    public company: CompanyEntity = new CompanyEntity();
+    public loading: boolean = false;
+
+    public constructor(
+        public sideMenuService: SideMenuService,
+        public breadcrumb: BreadcrumbService,
+        public companyService: CompanyService,
+        public storage: Storage,
+        public toast: ToastService
+    ) {}
 
     public ngOnInit() {
+        this.company = this.storage.getCompany() as CompanyEntity;
         this.breadcrumb.items = [
             {
                 name: `Início`,
                 route: undefined
             },
             {
-                name: `Meus usuários`,
+                name: `Minha empresa`,
                 route: `/dashboard/my-company`
             }
         ];
         this.sideMenuService.change(`my-company`);
+    }
+
+    public async save(): Promise<void> {
+        try {
+            this.loading = true;
+            const request: CompanyRequest = {
+                id: this.company.id,
+                name: this.company.name,
+                phone: this.company.phone,
+                revenueRecord: this.company.revenueRecord,
+                email: this.company.email
+            };
+            await this.companyService.update(request);
+            this.toast.success(`Informações salvas!`, `Sucesso!`);
+        } catch (ex: any) {
+            ex = ex as ZoppyException;
+            this.toast.error(ex.message, 'Não foi possível salvas as informações');
+        } finally {
+            this.loading = false;
+        }
     }
 }
---
+++ src/shared/models/requests/company/company.request.ts
@@ -0,0 +1,7 @@
+export class CompanyRequest {
+    public id?: string;
+    public name?: string;
+    public phone?: string;
+    public revenueRecord?: string;
+    public email?: string;
+}
