a082d54333057d6e6639cb0b8d2696ac795fa51c
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-09-01 19:37:33 -0300

    My company integration completed

--- src/shared/services/company/company.service.spec.ts
+++ src/shared/services/company/company.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CompanyService } from './company.service';
 
 describe('Service: Company', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
             providers: [CompanyService]
         });
     });
