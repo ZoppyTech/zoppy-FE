7685e9aa7e2a7324872d8455083b043281700abb
Daniel Cardoso <danielcardosoleao@gmail.com>
2022-09-14 20:37:34 -0300

    Fix whatsapp-account-service test

--- src/shared/services/whatsapp-account/whatsapp-account.service.spec.ts
+++ src/shared/services/whatsapp-account/whatsapp-account.service.spec.ts
@@ -1,15 +1,10 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { WhatsappAccountService } from './whatsapp-account.service';
 
 describe('WhatsappAccountService', () => {
-    let service: WhatsappAccountService;
-
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(WhatsappAccountService);
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule, RouterTestingModule],
             providers: [WhatsappAccountService]
