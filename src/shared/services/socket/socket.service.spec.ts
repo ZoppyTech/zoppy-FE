aaec389e8e6ed21878a45fcc6cebe26dcda81eca
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-09-14 20:06:56 -0300

    Fixing socket service tests

--- src/shared/services/socket/socket.service.spec.ts
+++ src/shared/services/socket/socket.service.spec.ts
@@ -1,12 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Socket } from 'ngx-socket-io';
 import { SocketService } from './socket.service';
 
 describe('Service: Socket', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [SocketService]
+            providers: [SocketService, { provide: Socket, useClass: SocketStub }]
         });
     });
 
@@ -14,3 +15,5 @@ describe('Service: Socket', () => {
         expect(service).toBeTruthy();
     }));
 });
+
+class SocketStub {}
