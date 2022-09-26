158071295d9f9d593a6ed70296d2b553c3781ac3
Daniel Cardoso <danielcardosoleao@gmail.com>
2022-09-25 21:18:23 -0300

    Add new templates to sent

--- src/core/pages/dashboard/whatsapp/utils/whatsapp.util.ts
+++ src/core/pages/dashboard/whatsapp/utils/whatsapp.util.ts
@@ -1,3 +1,5 @@
+import { WhatsappConstants } from 'src/shared/constants/whatsapp.constants';
+import { ChatRoom } from '../models/chat-room';
 import { ThreadMessage } from '../models/thread-message';
 
 export class WhatsappUtil {
@@ -17,5 +19,21 @@ export class WhatsappUtil {
             1;
         threads.reverse();
         return elementIndex;
+    }
+
+    public static getMessageTemplateParams(templateName: string, chatRoom: ChatRoom): Array<string> {
+        switch (templateName) {
+            case WhatsappConstants.MessageTemplates.GREETINGS_TO_USER:
+                return [chatRoom.contact.name, chatRoom.manager.name];
+            case WhatsappConstants.MessageTemplates.WELCOME_TO_THE_COMPANY:
+                return [chatRoom.account.businessName];
+            case WhatsappConstants.MessageTemplates.SAMPLE_ISSUE_RESOLUTION:
+                return [chatRoom.contact.name];
+            case WhatsappConstants.MessageTemplates.OUT_OF_BUSINESS_HOURS:
+                return [chatRoom.account.businessName, '9h', '18h'];
+            case WhatsappConstants.MessageTemplates.WAITING_A_FEW_MINUTES:
+            default:
+                return [];
+        }
     }
 }
