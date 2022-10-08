2bd5e9e754f2d53711f41e8657df0c5815466c6e
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-10-07 21:43:04 -0300

    Wip creating reports page

--- src/core/pages/dashboard/reports/components/matrix-rfm/matrix-rfm.component.ts
+++ src/core/pages/dashboard/reports/components/matrix-rfm/matrix-rfm.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-matrix-rfm',
     templateUrl: './matrix-rfm.component.html',
-    styleUrls: ['./matrix-rfm.component.css']
+    styleUrls: ['./matrix-rfm.component.scss']
 })
 export class MatrixRfmComponent implements OnInit {
     public constructor() {}
