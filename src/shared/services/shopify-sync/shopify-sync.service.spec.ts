8a5ce65285c3457093ef8cb0f838c6adaa33725c
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-11-21 09:19:38 -0300

    WIP integration shopify sync with frontend

--- src/shared/services/shopify-sync/shopify-sync.service.spec.ts
+++ src/shared/services/shopify-sync/shopify-sync.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ShopifySyncService } from './shopify-sync.service';
 
 describe('Service: ShopifySync', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
             providers: [ShopifySyncService]
         });
     });
