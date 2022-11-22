2320bd953abf9e281df00cb79a54cc65fbee3e92
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-11-22 14:21:54 -0300

    Fix order configuration

--- src/core/pages/dashboard/configuration/configuration.module.ts
+++ src/core/pages/dashboard/configuration/configuration.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
             {
                 path: '',
                 pathMatch: 'full',
-                redirectTo: 'access-keys'
+                redirectTo: 'access-tokens'
             },
             {
                 path: 'access-keys',
