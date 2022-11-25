8ba82777f3d08f79959d631085994c438692ac0e
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-11-25 13:58:25 -0300

    Finished feature update products

---
+++ src/core/pages/dashboard/products/product/product.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+import { IconModule } from '@ZoppyTech/icon';
+import { Routes, RouterModule } from '@angular/router';
+import { ButtonModule } from '@ZoppyTech/button';
+import { DropdownModule } from '@ZoppyTech/dropdown';
+import { InputModule } from '@ZoppyTech/input';
+import { DatepickerModule } from '@ZoppyTech/datepicker';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ProductComponent
+    }
+];
+
+@NgModule({
+    imports: [CommonModule, IconModule, RouterModule.forChild(routes), DropdownModule, InputModule, ButtonModule, DatepickerModule],
+    declarations: [ProductComponent]
+})
+export class ProductModule {}
---
+++ src/shared/models/requests/crm/crm-product.request.ts
@@ -0,0 +1,7 @@
+export interface CrmProductRequest {
+    id?: string;
+    categories?: Array<any>;
+    price?: number;
+    name?: string;
+    specification?: string;
+}
