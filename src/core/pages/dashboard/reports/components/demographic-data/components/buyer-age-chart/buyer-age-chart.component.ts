7ef48dfa7b53f1b6bedf9384f6e3c010bbfd5cd5
danleao <daniel.leao@zoppy.com.br>
2022-12-01 13:27:40 -0300

    Adjustment sales by gender chart

--- src/core/pages/dashboard/reports/components/demographic-data/components/buyer-age-chart/buyer-age-chart.component.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/buyer-age-chart/buyer-age-chart.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'buyer-age-chart',
     templateUrl: './buyer-age-chart.component.html',
     styleUrls: ['./buyer-age-chart.component.scss']
 })
-export class BuyerAgeChartComponent {}
+export class BuyerAgeChartComponent {
+    public isLoading: boolean = false;
+    public logo: string = `${environment.publicBucket}/imgs/loading.svg`;
+
+    public barChartOptions: any = {
+        scaleShowVerticalLines: false,
+        responsive: true,
+        indexAxis: 'x',
+        backgroundColor: ['#E3D6FD', '#f3f3f3'],
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
+    public barChartLabels: string[] = ['a', 'b'];
+    public barChartType: string = 'bar';
+    public barChartLegend: boolean = false;
+    public barChartData: any[] = [{ data: [70, 30], label: 'Idade do público comprador' }];
+
+    public ngOnInit(): void {}
+}
--- src/core/pages/dashboard/reports/components/demographic-data/components/sales-by-gender-chart/sales-by-gender-chart.component.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/sales-by-gender-chart/sales-by-gender-chart.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'sales-by-gender-chart',
     templateUrl: './sales-by-gender-chart.component.html',
     styleUrls: ['./sales-by-gender-chart.component.scss']
 })
-export class SalesByGenderChartComponent {}
+export class SalesByGenderChartComponent implements OnInit {
+    public isLoading: boolean = false;
+    public logo: string = `${environment.publicBucket}/imgs/loading.svg`;
+    public legends: Legend[] = [];
+
+    public doughnutChartOptions: any = {
+        scaleShowVerticalLines: false,
+        responsive: true,
+        indexAxis: 'y',
+        backgroundColor: ['#E3D6FD', '#f3f3f3'],
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
+    public doughnutChartLabels: string[] = ['a', 'b'];
+    public doughnutChartType: string = 'doughnut';
+    public doughnutChartLegend: boolean = false;
+    public doughnutChartData: any[] = [{ data: [70, 30], label: 'Compras por gênero' }];
+
+    public ngOnInit(): void {
+        this.setLegend();
+    }
+
+    public setLegend(): void {
+        this.legends = [
+            {
+                value: 'Feminino',
+                color: '#E3D6FD'
+            },
+            {
+                value: 'Masculino',
+                color: '#D0F0FD'
+            },
+            {
+                value: 'Não registrado',
+                color: '#CAD3E1'
+            }
+        ];
+    }
+}
+
+interface Legend {
+    color: string;
+    value: string;
+}
