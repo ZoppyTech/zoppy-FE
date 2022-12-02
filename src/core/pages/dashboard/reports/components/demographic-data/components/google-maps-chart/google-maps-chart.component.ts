4b03467e48d631fe5a903b855e4c95a6cf328ef4
danleao <114452764+danleao@users.noreply.github.com>
2022-12-02 15:20:59 -0300

    Feature/dashboard v2 (#73)
    * Create RFM overview component
    
    * Fix RFM page layout
    
    * Create ABC Curve and Nps page
    
    * Create cards for ABC curve page
    
    * Create demographic data page
    
    * Add new charts for demographic data page
    
    * Adjustment sales by gender chart
    
    * Add google maps

--- src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.component.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.component.ts
@@ -1,8 +1,69 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MapGeocoder, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
     selector: 'google-maps-chart',
     templateUrl: './google-maps-chart.component.html',
     styleUrls: ['./google-maps-chart.component.scss']
 })
-export class GoogleMapsChartComponent {}
+export class GoogleMapsChartComponent implements OnInit, AfterViewInit {
+    public display: any;
+    public center: google.maps.LatLngLiteral = {
+        lat: -19.912998,
+        lng: -43.940933
+    };
+    public zoom = 4;
+    public mapInfoContent: string = 'Amigo estou aqui! :)';
+    @ViewChild(MapInfoWindow) public infoWindow: MapInfoWindow | undefined;
+
+    public markerPositions: google.maps.LatLngLiteral[] = [];
+
+    public constructor(public geocoder: MapGeocoder) {}
+
+    // public addMarker(event: google.maps.MapMouseEvent) {
+    //     if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+    // }
+
+    public openInfoWindow(marker: MapMarker) {
+        if (this.infoWindow != undefined) this.infoWindow.open(marker);
+    }
+
+    public ngOnInit(): void {}
+
+    public async onSearchTextChanged(searchText: string = ''): Promise<void> {
+        this.center = { lat: -33.8666, lng: 151.1958 };
+        const result: any = await this.geocoder
+            .geocode({
+                address: searchText
+            })
+            .subscribe(({ results }: any) => {
+                this.center = { lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng() };
+                this.markerPositions = [];
+                this.markerPositions.push(this.center);
+                this.zoom = 15;
+            });
+        console.log(result);
+    }
+
+    public async ngAfterViewInit(): Promise<void> {}
+
+    // public updateMapGeoLocation(lat: number, lng: number): void {
+    //     this.center = {
+    //         lat: lat,
+    //         lng: lng
+    //     };
+    // }
+
+    public moveMap(event: google.maps.MapMouseEvent): void {
+        if (event.latLng != null) {
+            console.log(event.latLng.toJSON());
+            this.display = event.latLng.toJSON();
+        }
+    }
+
+    public move(event: google.maps.MapMouseEvent): void {
+        if (event.latLng != null) this.display = event.latLng.toJSON();
+    }
+}
+
+class MapEmptyObject extends HTMLElement {}
--- src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.module.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { GoogleMapsChartComponent } from './google-maps-chart.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { SearchBarModule } from '@ZoppyTech/search-bar';
 
 @NgModule({
     declarations: [GoogleMapsChartComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, NgChartsModule],
+    imports: [CommonModule, GoogleMapsModule, FormsModule, ReactiveFormsModule, NgChartsModule, SearchBarModule],
     exports: [GoogleMapsChartComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
