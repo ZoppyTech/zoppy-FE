606c681aabda3ff08e6c644f3e4e2a8a823a31dd
lucasroscoezoppy <114440805+lucasroscoezoppy@users.noreply.github.com>
2023-01-06 00:00:54 -0300

    Development (#102)
    * Conclude user profiles
    
    * Conclude user profiles (#96)
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * finish user profiles
    
    * Feature/user profiles (#97)
    
    * Conclude user profiles
    
    * finish user profiles
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * add giftback config to user profiles
    
    * whatsapp shared all profiles
    
    * Fix monthly reports (#99)
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * Conclude curve ABC implementation (#101)
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>

--- src/core/pages/dashboard/reports/components/nps/components/abc-curve-chart/abc-curve-chart.module.ts
+++ src/core/pages/dashboard/reports/components/nps/components/abc-curve-chart/abc-curve-chart.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from '@ZoppyTech/dropdown';
+import { NgChartsModule } from 'ng2-charts';
 import { AbcCurveChartComponent } from './abc-curve-chart.component';
 
 @NgModule({
     declarations: [AbcCurveChartComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, DropdownModule, FormsModule, ReactiveFormsModule, NgChartsModule],
     exports: [AbcCurveChartComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
