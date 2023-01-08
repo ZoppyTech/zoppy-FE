e122f59169c68b9de19d69feaef8895a8f55c0c2
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-01-07 23:23:23 -0300

    Improve sales panel

--- src/shared/components/modal/sales-panel-contact/sales-panel-contact.component.spec.ts
+++ src/shared/components/modal/sales-panel-contact/sales-panel-contact.component.spec.ts
@@ -20,8 +20,4 @@ describe('SalesPanelContactComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
 });
