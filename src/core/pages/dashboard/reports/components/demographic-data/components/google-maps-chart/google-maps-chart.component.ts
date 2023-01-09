37ea64d5e6c9f07d3abe3f2c6b1f57bf27f1f0dc
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-01-09 15:49:42 -0300

    fix map layout

--- src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.component.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.component.ts
@@ -33,6 +33,22 @@ export class GoogleMapsChartComponent implements OnInit, OnDestroy, AfterViewIni
     public zoom = 4;
     public isLoading: boolean = true;
     public logo: string = `${environment.publicBucket}/imgs/loading.svg`;
+    public gradient: Array<string> = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ];
 
     public constructor(
         public geocoder: MapGeocoder,
@@ -75,8 +91,12 @@ export class GoogleMapsChartComponent implements OnInit, OnDestroy, AfterViewIni
             this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, this.mapOptions);
             this.heatmap = new google.maps.visualization.HeatmapLayer({
                 data: this.getPoints(),
-                map: this.map
+                map: this.map,
+                radius: 20,
+                opacity: 1,
+                gradient: this.gradient
             });
+            this.heatmap.setMap(this.map);
         });
     }
 
