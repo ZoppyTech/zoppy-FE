d49888296ae00d22116fccfa3fde876ae5570aef
danleao <daniel.leao@zoppy.com.br>
2023-01-11 02:32:08 -0300

    Add float buttom to show markers

--- src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.module.ts
+++ src/core/pages/dashboard/reports/components/demographic-data/components/google-maps-chart/google-maps-chart.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { ButtonModule } from '@ZoppyTech/button';
+import { IconModule } from '@ZoppyTech/icon';
 import { SearchBarModule } from '@ZoppyTech/search-bar';
 import { GoogleMapsChartComponent } from './google-maps-chart.component';
 
 @NgModule({
     declarations: [GoogleMapsChartComponent],
-    imports: [CommonModule, GoogleMapsModule, SearchBarModule, ButtonModule],
+    imports: [CommonModule, GoogleMapsModule, SearchBarModule, ButtonModule, IconModule],
     exports: [GoogleMapsChartComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
