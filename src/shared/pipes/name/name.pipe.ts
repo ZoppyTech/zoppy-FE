578a2ded4d0af054c4a88971fffeae7201004819
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-01-13 11:23:00 -0300

    final adjustments on names

---
+++ src/shared/pipes/name/name.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { FormatUtils } from 'src/shared/utils/format.util';
+
+@Pipe({
+    name: 'zoppyName'
+})
+export class NamePipe implements PipeTransform {
+    public transform(value: string, args?: any): any {
+        const names: string[] = value.split(' ');
+        const response: string[] = [];
+
+        for (const splitName of names) {
+            if (!splitName[0]) continue;
+            response.push(splitName[0].toUpperCase() + splitName.substring(1, splitName.length).toLowerCase());
+        }
+
+        return response.join(' ');
+    }
+}
