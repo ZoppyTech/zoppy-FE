4a3367ba192c1ab1deaabc02c7eb0e62b0ede800
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-02-18 06:53:24 -0300

    conclude register API and clean methods

--- src/shared/services/yampi-sync/yampi-sync.service.spec.ts
+++ src/shared/services/yampi-sync/yampi-sync.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { WhatsappContactService } from '../whatsapp-contact/whatsapp-contact.service';
 
 import { YampiSyncService } from './yampi-sync.service';
 
@@ -6,7 +9,10 @@ describe('YampiSyncService', () => {
     let service: YampiSyncService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+            providers: [WhatsappContactService]
+        });
         service = TestBed.inject(YampiSyncService);
     });
 
