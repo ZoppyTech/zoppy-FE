cf6982eaa9789f8d786c58780d414f8a3ec3a78c
danleao <114452764+danleao@users.noreply.github.com>
2023-04-05 02:56:57 -0300

    Development (#173)
    * one-chat: WIP creating provider
    
    * conclude improvements on giftback configs (#160)
    
    * Adjustment chat layout (#161)
    
    * Add float button
    
    * Change px to rem
    
    * Add border in pull customers button
    
    * changing colors of chat components
    
    * Adjustment contact list page for chat
    
    * Adjustment chat v3
    
    * Fix add button click
    
    * one-chat: creating front-end logic
    
    * Adjustment chat layout v3
    
    * Conclude message templates (#163)
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * reducing 'customers in queue' screen
    
    * Adjustment contact list header
    
    * Change px to rem
    
    * nuvemshop-category: adding nuvemshop as category option
    
    * finish message template
    
    * Feature/chat v3 (#167)
    
    * Adjustment chat layout
    
    * Fix create whatsapp accounts
    
    * Impl fileConverter function
    
    * Change document icon color
    
    * Create Countdown Timer component
    
    * Implements AfterViewInit in CountdownTimerComponent
    
    * Feature/abandoned cart (#168)
    
    * wip abandoned cart
    
    * Improvements on social media logic, add task util from abandoned cart
    
    ---------
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * Fix countdowntimer component
    
    * Impl finish conversation
    
    * Adjustment bubble info component
    
    * Listen 'update_current_chat_room' chat event
    
    * Fix bugs
    
    * Fix bugs
    
    * one-chat: fixing limitations
    
    * one-chat: fixing customer sync
    
    * Feature/checkout (#170)
    
    * wip abandoned cart
    
    * Improvements on social media logic, add task util from abandoned cart
    
    * wip improvements on checkout flow
    
    * finish new checkout
    
    ---------
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * adjust details on page
    
    * adjust loading
    
    * fix theme
    
    * Fix bugs (#171)
    
    * Show message owner name in thread messages
    
    * Add contact about panel
    
    ---------
    
    Co-authored-by: pedro-zoppy <pedro.bortolini@zoppy.com.br>
    Co-authored-by: Lucas Roscoe <30293662+lucasroscoeoliveira@users.noreply.github.com>
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    Co-authored-by: lucasroscoezoppy <114440805+lucasroscoezoppy@users.noreply.github.com>
    Co-authored-by: pedro-zoppy <119454845+pedro-zoppy@users.noreply.github.com>

--- src/core/pages/dashboard/whatsapp/components/chat-room/chat-room.module.ts
+++ src/core/pages/dashboard/whatsapp/components/chat-room/chat-room.module.ts
@@ -14,6 +14,7 @@ import { AudioMessageModule } from './components/audio-message/audio-message.mod
 import { DocumentMessageModule } from './components/document-message/document-message.module';
 import { StaticLoadingModule } from 'src/shared/components/static-loading/static-loading.module';
 import { CountdownTimerModule } from './components/countdown-timer/countdown-timer.module';
+import { ContactInfoPanelModule } from './components/contact-info-panel/contact-info-panel.module';
 
 const providers: Provider[] = [ChatUtility];
 
@@ -32,7 +33,8 @@ const providers: Provider[] = [ChatUtility];
         AudioMessageModule,
         DocumentMessageModule,
         StaticLoadingModule,
-        CountdownTimerModule
+        CountdownTimerModule,
+        ContactInfoPanelModule
     ],
     providers: [providers],
     exports: [ChatRoomComponent],
