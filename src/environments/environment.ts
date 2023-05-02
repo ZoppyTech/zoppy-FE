0bf5fc4d12313520058a4615b1f39adbe56fc0d2
danleao <daniel.leao@zoppy.com.br>
2023-05-02 13:54:05 -0300

    Add new apiPartnersUrl field

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment: any = {
     appUrl: 'http://localhost:4200',
     apiUrl: 'http://localhost:8080',
     partnersUrl: 'https://partners-dev.zoppy.com.br',
+    apiPartnersUrl: 'http://localhos:8082',
     publicBucket: 'https://zoppy-public-dev.s3.amazonaws.com',
     support: 'https://wa.me/message/7XWZOLXUFLNOM1'
 };
