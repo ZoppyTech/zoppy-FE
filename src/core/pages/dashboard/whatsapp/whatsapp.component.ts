fb1fb0adab34f06112f606d5e255352b41f6d521
danleao <114452764+danleao@users.noreply.github.com>
2023-05-01 21:00:35 -0300

    Development (#192)
    * Feature/refactoring chat (#189)
    
    * Adjustment chat countdown timer layout
    
    * Mapping and refactoring chat list component
    
    * Fix finish conversation operation
    
    * Fix list contacts in modal
    
    * Create addRoom function in chat handler class
    
    * Fix transfer and finished conversation
    
    * Fix events: update manager room, finished room and tranfer room
    
    * Remove debugger and comments
    
    ---------
    
    Co-authored-by: danleao <daniel.cardoso@fusiondms.com.br>
    
    * Fix display user name
    
    * Fix countdown timer
    
    * Remove debugger of code
    
    * Feature/message templates (#190)
    
    * Conclude message templates
    
    * wip message templates
    
    * wip message templates
    
    * fix wpp link on sales panel
    
    * test
    
    * test
    
    * wip message templates
    
    * return with batch upload data
    
    * wip message templates
    
    * return with sync data page only for master users
    
    * starting to look good with the new templates
    
    * wip message templates
    
    * wip message templates
    
    * WIP message templates
    
    * conclude initial version of message templates
    
    * finish message templates
    
    ---------
    
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    
    * improve code quality
    
    * adjust template input
    
    * adjust modal layout
    
    * Adjustment truncate message text
    
    * Fix update room manager if send template message
    
    ---------
    
    Co-authored-by: danleao <daniel.cardoso@fusiondms.com.br>
    Co-authored-by: lucasroscoezoppy <114440805+lucasroscoezoppy@users.noreply.github.com>
    Co-authored-by: lucasroscoeoliveira <lucasrloliveira@gmail.com>
    Co-authored-by: Lucas Roscoe <lucas.roscoe@zoppy.com.br>

--- src/core/pages/dashboard/whatsapp/whatsapp.component.ts
+++ src/core/pages/dashboard/whatsapp/whatsapp.component.ts
@@ -174,12 +174,11 @@ export class WhatsappComponent implements OnInit, OnDestroy {
 
             if (thread.type === WhatsappConstants.MessageType.Template) {
                 const request: WhatsappTemplateMessageRequest = this.buildTemplateMessageRequestFrom(thread);
-                console.log(request);
                 await this.wppMessageService.createTemplateMessage(request);
-                return;
+            } else {
+                const request: WhatsappTextMessageRequest = this.buildTextMessageRequestFrom(thread);
+                await this.wppMessageService.createTextMessage(request);
             }
-            const request: WhatsappTextMessageRequest = this.buildTextMessageRequestFrom(thread);
-            await this.wppMessageService.createTextMessage(request);
 
             if (!this.roomSelected.manager) {
                 this.chathandler.updateRoomManager(this.roomSelected);
