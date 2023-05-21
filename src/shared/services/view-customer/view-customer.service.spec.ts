ef37d3f0acc9e0569cd9d575179b36c32e6463d1
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-05-21 18:28:28 -0300

    adjust tests

--- src/shared/services/view-customer/view-customer.service.spec.ts
+++ src/shared/services/view-customer/view-customer.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ViewCustomerService } from './view-customer.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ViewCustomerService', () => {
     let service: ViewCustomerService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [ViewCustomerService],
+            imports: [HttpClientTestingModule, RouterTestingModule]
+        });
         service = TestBed.inject(ViewCustomerService);
     });
 
